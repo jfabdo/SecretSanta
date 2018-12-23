ff4c423092e9b69d38da0f4db5f3e05df2e6d038
Jack Abdo <jfabdo@gmail.com>
2018-12-23 03:11:15 -0700

    Used position based comparision.

--- secretsanta.py
+++ secretsanta.py
@@ -8,20 +8,27 @@ class SecretSanta:
     def match_santas(self):
        #secsantalist will contain an unchecked list of secret santas
        poplen = len(self.group)  # population length
-       secsantalist = sample(self.group,poplen) 
+       secsantalist = sample(range(poplen),poplen) #sample(self.group,poplen) 
+       
        
        #check and avoid collisions based on string comparisons
        for i in range(poplen):
-         while self.group[i] == secsantalist[i]:
-           j = i % poplen
-           k = (j + 1) % poplen
+         if i == secsantalist[i]:
+           j = secsantalist[i+1]
+           secsantalist[i+1] = i
+           secsantalist[i] = j
+           #j = i % poplen
+           #k = (j + 1) % poplen
            
-           tempsanta = secsantalist[k]
-           secsantalist[k] = secsantalist[j]
-           secsantalist[j] = tempsanta
-           i = (i + 1) % poplen
+           #tempsanta = secsantalist[k]
+           #secsantalist[k] = secsantalist[j]
+           #secsantalist[j] = tempsanta
+           #i = (i + 1) % poplen
+       finalsantalist = []
+       for i in range(poplen):
+         finalsantalist.append(self.group[secsantalist[i]])
        
-       return dict(zip(self.group,secsantalist))
+       return dict(zip(self.group,finalsantalist))
     
     self.Matches = match_santas(self)
     
